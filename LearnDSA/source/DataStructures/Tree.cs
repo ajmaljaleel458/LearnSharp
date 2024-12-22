dfa5faa58dd6c1b81fe2eafef74612e0757115c3
ajmaljaleel458 <ajmaljaleel.official.458@gmail.com>
2024-12-22 15:02:38 +0530

    Convert SOme of the Tree Manupulation methods from static to member --ajmaljaleel458

--- LearnDSA/source/DataStructures/Tree.cs
+++ LearnDSA/source/DataStructures/Tree.cs
@@ -3,6 +3,13 @@
     using Logger;
     public class Tree
     {
+        private Node? _root;
+
+        public Node? Root
+        {
+            get { return _root; }
+        }
+
         public static int GetMaxNumOfNodeAtNLevel(int level)
         {
             return (int)Math.Pow(2, level);
@@ -43,88 +50,16 @@
             }
         }
 
-        public static void TreeSandBox()
+        public void Insert(int data)
         {
-            Node firstNode = new Node(1);
-            Node secondNode = new Node(2);
-            Node thirdNode = new Node(3);
-            Node fourthNode = new Node(4);
-            Node fifththNode = new Node(5);
-            Node sixthNode = new Node(6);
-            Node seventhNode = new Node(7);
-
-            firstNode.Left = secondNode;
-            firstNode.Right = thirdNode;
-
-            secondNode.Left = fourthNode;
-            secondNode.Right = fifththNode;
-
-            thirdNode.Left = sixthNode;
-            thirdNode.Right = seventhNode;
-
-            Tree.Insert(firstNode, 100);
-
-            LevelOrderTraversal(firstNode);
-
-            Logger.Info($"{5} is {(Tree.Search(firstNode, 5) ? "found" : "not-found")}");
-            Tree.Delete(firstNode, 5);
-            Logger.Info($"{5} is {(Tree.Search(firstNode, 5) ? "found" : "not-found")}");
-        }
-
-        #region Tree Traversal Algorithms
-
-        #region Depth First Search
-        public static void PreOrderTraversal(Node? node)
-        {
-            if (node == null) return;
-            Logger.Info($"Data : {node.Data}");
-            PreOrderTraversal(node.Left);
-            PreOrderTraversal(node.Right);
-        }
-        public static void InOrderTraversal(Node? node)
-        {
-            if (node == null) return;
-            PreOrderTraversal(node.Left);
-            Logger.Info($"Data : {node.Data}");
-            PreOrderTraversal(node.Right);
-        }
-        public static void PostOrderTraversal(Node? node)
-        {
-            if (node == null) return;
-            PreOrderTraversal(node.Left);
-            PreOrderTraversal(node.Right);
-            Logger.Info($"Data : {node.Data}");
-        }
-        #endregion
-
-        #region Breadth First Search
-        public static void LevelOrderTraversal(Node? node)
-        {
-            if (node == null) return;
-
-            Queue<Node> queue = new Queue<Node>();
-            queue.Enqueue(node);
-
-            while (queue.Count > 0)
+            if (_root == null)
             {
-                Node? current = queue.Dequeue();
-
-                Logger.Info($"Data : {current.Data}");
-                if (current.Left != null) queue.Enqueue(current.Left);
-                if (current.Right != null) queue.Enqueue(current.Right);
+                _root = new Node(data);
+                return;
             }
-        }
-        #endregion
-        #endregion
-
-        #region Tree Insersion Algorithms
-        public static Node? Insert(Node? root, int data)
-        {
-            if (root == null)
-                return new Node(data);
 
             Queue<Node> queue = new Queue<Node>();
-            queue.Enqueue(root);
+            queue.Enqueue(_root);
 
             while (queue.Count > 0)
             {
@@ -150,37 +85,32 @@
                     queue.Enqueue(temp.Right);
                 }
             }
-            return root;
         }
-        #endregion
 
-        #region Tree Search Algorithms
-        #region Deapth First Search
-        public static bool Search(Node? root, int data)
+        public void LevelOrderTraversal()
         {
-            if (root == null) return false;
+            if (_root == null) return;
 
-            // If the node's data is equal to the value we are
-            // searching for
-            if (root.Data == data) return true;
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(_root);
 
-            // Recursively search in the Left and Right subtrees
-            bool left_res = Search(root.Left, data);
-            bool right_res = Search(root.Right, data);
+            while (queue.Count > 0)
+            {
+                Node? current = queue.Dequeue();
 
-            return left_res || right_res;
+                Logger.Info($"Data : {current.Data}");
+                if (current.Left != null) queue.Enqueue(current.Left);
+                if (current.Right != null) queue.Enqueue(current.Right);
+            }
         }
-        #endregion
-        #endregion
 
-        #region Tree Deletion Algorithm
-        public static Node? Delete(Node? root, int val)
+        public void Delete(int data)
         {
-            if (root == null) return null;
+            if (_root == null) return;
 
-            // Use a queue to perform BFS
             Queue<Node?> q = new Queue<Node?>();
-            q.Enqueue(root);
+            q.Enqueue(_root);
+
             Node? target = null;
 
             // Find the target node
@@ -188,7 +118,7 @@
             {
                 Node? curr = q.Dequeue();
 
-                if (curr.Data == val)
+                if (curr.Data == data)
                 {
                     target = curr;
                     break;
@@ -196,15 +126,15 @@
                 if (curr.Left != null) q.Enqueue(curr.Left);
                 if (curr.Right != null) q.Enqueue(curr.Right);
             }
-            if (target == null) return root;
+            if (target == null) return;
 
             // Find the deepest rightmost node and its parent
             Node? lastNode = null;
             Node? lastParent = null;
             Queue<(Node?, Node?)> q1 = new Queue<(Node?, Node?)>();
-            q1.Enqueue((root, null));
+            q1.Enqueue((_root, null));
 
-            while (q1.Count > 0)    
+            while (q1.Count > 0)
             {
                 var (curr, parent) = q1.Dequeue();
                 lastNode = curr;
@@ -225,11 +155,66 @@
             }
             else
             {
-                return null;
+                return;
             }
-            return root;
+            return;
         }
-        #endregion
+
+        public bool Search(Node? root, int data)
+        {
+            if (root == null) return false;
+
+            if (root.Data == data) return true;
+
+            // Recursively search in the Left and Right subtrees
+            bool left_res = Search(root.Left, data);
+            bool right_res = Search(root.Right, data);
+
+            return left_res || right_res;   
+        }
+
+        public static void TreeSandBox()
+        {
+            Tree tree = new Tree();
+
+            tree.Insert(10);
+            tree.Insert(20);
+            tree.Insert(30);
+            tree.Insert(40);
+            tree.Insert(50);
+
+            tree.Delete(30);
+
+            tree.LevelOrderTraversal();
+
+            Logger.Info($"{10} is {(tree.Search(tree.Root, 10) ? "found" : "not-found")}");
+            Logger.Info($"{100} is {(tree.Search(tree.Root, 100) ? "found" : "not-found")}");
+        }
+
+
+        public static void PreOrderTraversal(Node? node)
+        {
+            if (node == null) return;
+            Logger.Info($"Data : {node.Data}");
+            PreOrderTraversal(node.Left);
+            PreOrderTraversal(node.Right);
+        }
+        public static void InOrderTraversal(Node? node)
+        {
+            if (node == null) return;
+            PreOrderTraversal(node.Left);
+            Logger.Info($"Data : {node.Data}");
+            PreOrderTraversal(node.Right);
+        }
+        public static void PostOrderTraversal(Node? node)
+        {
+            if (node == null) return;
+            PreOrderTraversal(node.Left);
+            PreOrderTraversal(node.Right);
+            Logger.Info($"Data : {node.Data}");
+        }
+
+        
     }
 
 
