6a3019796e12d47826c7450a5da3800015917ec8
ajmaljaleel458 <ajmaljaleel.official.458@gmail.com>
2024-12-28 11:05:16 +0530

    Storing a list of student names: Store and display the names of students in a class using an array.

---
+++ LearnSharp/source/Collections/Array.cs
@@ -0,0 +1,48 @@
+﻿namespace LearnSharp.Collections
+{
+    using Logger;
+    using System;
+
+    public class StudentLog
+    {
+        private string[] _studentNames;
+
+        private int _numberOfStudents;
+
+        public delegate void StudentIterator(string name);
+
+        public StudentLog(int capacity)
+        {
+            _studentNames = new string[capacity];
+
+            _numberOfStudents = 0;
+        }
+
+        public void Insert(string name)
+        {
+            _studentNames[_numberOfStudents] = name;
+            _numberOfStudents++;
+        }
+
+        public void EachStudent(StudentIterator onEachStudent)
+        {
+            for (int index = 0; index < _numberOfStudents; index++)
+                onEachStudent(_studentNames[index]);    
+        }
+    }
+
+    public class Array
+    {   
+        public static void SandBox()
+        {
+            StudentLog studentLog = new StudentLog(5);
+
+            studentLog.Insert("Ajmal");
+            studentLog.Insert("Asiya");
+            studentLog.Insert("Haris");
+            studentLog.Insert("Akumal");
+
+            studentLog.EachStudent((string name) => Logger.Info($"Name : {name}"));
+        }
+    }
+}
